edd10a8b76c3142f478e4fc84959369e2dac2ae0
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2019-05-23 00:02:16 -0300

    Ajustes para correção de problema no build

--- app/src/main/java/com/gustavobarbosa/fooddelivery/ui/MainActivity.kt
+++ app/src/main/java/com/gustavobarbosa/fooddelivery/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package com.gustavobarbosa.fooddelivery.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.gustavobarbosa.fooddelivery.R
+import com.gustavobarbosa.fooddelivery.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = HomeFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.container,fragment).commit()
     }
 }
