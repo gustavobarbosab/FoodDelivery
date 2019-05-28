b503aec208f9391e621529a91c00c52b6bef06f1
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2019-05-27 22:51:35 -0300

    Ajustes nos botões de atalho

--- app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gustavobarbosa.fooddelivery.R
+import kotlinx.android.synthetic.main.content_button_food.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(), HomeContract.View {
 
@@ -22,7 +24,25 @@ class HomeFragment : Fragment(), HomeContract.View {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        btHamburguer.ivShortcutFood.setImageResource(R.drawable.ic_hamburguer)
+        btPasta.ivShortcutFood.setImageResource(R.drawable.ic_pasta)
+        btPizza.ivShortcutFood.setImageResource(R.drawable.ic_pizza)
+
+        btHamburguer.tvShortcutFoodName.text = "Hamburguer"
+        btPasta.tvShortcutFoodName.text = "Pasta"
+        btPizza.tvShortcutFoodName.text = "Pizza"
+
+        btHamburguer.tvShortcutFoodNamePrice.text = "R$ 12"
+        btPasta.tvShortcutFoodNamePrice.text = "R$ 13"
+        btPizza.tvShortcutFoodNamePrice.text = "R$ 16"
     }
 
 }
