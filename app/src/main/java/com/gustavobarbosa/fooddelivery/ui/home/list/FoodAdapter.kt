df03e7aa6614f606ffaa27a262168ae51634ddbd
Gustavo Barbosa <tqi_gbarreto@uolinc.com>
2019-05-30 15:50:58 -0300

    comunicação do recycler item com a activity

--- app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/list/FoodAdapter.kt
+++ app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/list/FoodAdapter.kt
@@ -4,20 +4,37 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.gustavobarbosa.fooddelivery.R
 
-class FoodAdapter: RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
+class FoodAdapter(private val listener: ClickAddFoodListener) : RecyclerView.Adapter<FoodAdapter.ViewHolder>() {
 
-    override fun getItemCount(): Int = 5
+    val arrayFood: List<String> = listOf("BBQ Burguer", "Cheedar Burguer", "Fit Burger")
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        //TOOD implementar isso
-    }
+    override fun getItemCount(): Int = arrayFood.size
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_food,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_food, parent, false)
         return ViewHolder(view)
     }
 
-    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = arrayFood[position]
+
+        holder.tvFoodName.text = item
+
+        holder.btAddFood.setOnClickListener {
+            listener.onFoodChoose(item)
+        }
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val btAddFood: Button = view.findViewById(R.id.buttonAddFood)
+        val tvFoodName: TextView = view.findViewById(R.id.tvItemFoodName)
+    }
+
+    interface ClickAddFoodListener {
+        fun onFoodChoose(foodName: String)
+    }
 }
--- app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/list/FoodListFragment.kt
+++ app/src/main/java/com/gustavobarbosa/fooddelivery/ui/home/list/FoodListFragment.kt
@@ -6,10 +6,17 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.gustavobarbosa.fooddelivery.R
 import kotlinx.android.synthetic.main.fragment_food_list.*
 
 class FoodListFragment: Fragment() {
+
+    private val foodSelectedListFragment = object :FoodAdapter.ClickAddFoodListener {
+        override fun onFoodChoose(foodName: String) {
+            Toast.makeText(context,foodName,Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +31,7 @@ class FoodListFragment: Fragment() {
     }
 
     private fun setupView() {
-        rvFood.adapter = FoodAdapter()
+        rvFood.adapter = FoodAdapter(foodSelectedListFragment)
         rvFood.layoutManager = LinearLayoutManager(this.context,LinearLayoutManager.HORIZONTAL,false)
     }
 }
