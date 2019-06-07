f9f93bfc83c2128ebb8ccaf3654311ef946caa82
Gustavo Barbosa <tqi_gbarreto@uolinc.com>
2019-06-07 18:06:54 -0300

    Adicionando testes usando Rx

--- app/src/test/java/com/gustavobarbosa/fooddelivery/ui/cart/CartPresenterTest.kt
+++ app/src/test/java/com/gustavobarbosa/fooddelivery/ui/cart/CartPresenterTest.kt
@@ -1,45 +1,43 @@
 package com.gustavobarbosa.fooddelivery.ui.cart
 
-import com.gustavobarbosa.fooddelivery.data.repository.ResponseListener
 import com.gustavobarbosa.fooddelivery.data.repository.food.FoodRepository
 import com.gustavobarbosa.fooddelivery.domain.model.FoodModel
+import com.gustavobarbosa.fooddelivery.ui.cart.utils.RxSchedulerRule
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
 import io.mockk.verify
 import io.mockk.verifyAll
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
 import org.junit.Before
+import org.junit.ClassRule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
-@RunWith(JUnit4::class)
 class CartPresenterTest {
 
     private val view: CartContract.View = mockk(relaxed = true)
 
     private val repository: FoodRepository = mockk(relaxed = true)
 
-    private val slotListener = slot<ResponseListener<ArrayList<FoodModel>>>()
+    private val compositeDisposable: CompositeDisposable = mockk(relaxed = true)
 
     private lateinit var cartPresenter: CartContract.Presenter
 
     @Before
     fun init() {
-        cartPresenter = CartPresenter(view, repository)
+        cartPresenter = CartPresenter(view, repository, compositeDisposable)
     }
 
     @Test
     fun `reload cart and notify view`() {
         every {
-            repository.getFoodCart(capture(slotListener))
-        } answers {
-            slotListener.captured.onSuccess(arrayListOf(
+            repository.getFoodCart()
+        } returns Single.just(
+            arrayListOf(
                 FoodModel("", 22.40, "", ""),
                 FoodModel("", 20.30, "", "")
             )
-            )
-        }
+        )
 
         cartPresenter.reloadCart()
 
@@ -53,10 +51,10 @@ class CartPresenterTest {
     @Test
     fun `reload cart with empty list and notify view`() {
         every {
-            repository.getFoodCart(capture(slotListener))
-        } answers {
-            slotListener.captured.onSuccess(arrayListOf())
-        }
+            repository.getFoodCart()
+        } returns Single.just(
+            arrayListOf()
+        )
 
         cartPresenter.reloadCart()
 
@@ -70,10 +68,12 @@ class CartPresenterTest {
     fun `remove item from cart`() {
         val model = FoodModel("", 22.0, "", "")
         every {
-            repository.removeFoodOfCart(model, capture(slotListener))
-        } answers {
-            slotListener.captured.onSuccess(arrayListOf(model))
-        }
+            repository.removeFoodOfCart(model)
+        } returns Single.just(
+            arrayListOf(
+                FoodModel("", 22.40, "", "")
+            )
+        )
 
         cartPresenter.removeItem(model)
 
@@ -88,10 +88,10 @@ class CartPresenterTest {
     fun `remove item from cart and empty cart`() {
         val model = FoodModel("", 22.0, "", "")
         every {
-            repository.removeFoodOfCart(model, capture(slotListener))
-        } answers {
-            slotListener.captured.onSuccess(arrayListOf())
-        }
+            repository.removeFoodOfCart(model)
+        } returns Single.just(
+            arrayListOf()
+        )
 
         cartPresenter.removeItem(model)
 
@@ -104,16 +104,20 @@ class CartPresenterTest {
     @Test
     fun `assert if view is destroyed`() {
         every {
-            repository.getFoodCart(capture(slotListener))
-        } answers {
-            slotListener.captured.onSuccess(arrayListOf(
-                FoodModel("", 22.40, "", "")
+            repository.getFoodCart()
+        } returns Single.just(
+            arrayListOf(
+                FoodModel("", 22.40, "", ""),
+                FoodModel("", 20.30, "", "")
             )
-            )
-        }
+        )
 
         cartPresenter.destroy()
         cartPresenter.reloadCart()
+
+        verify {
+            compositeDisposable.dispose()
+        }
 
         verify(exactly = 0) {
             view.hideButtonNext()
@@ -122,5 +126,11 @@ class CartPresenterTest {
             view.showButtonNext()
             view.updatePrice(any())
         }
+    }
+
+    companion object {
+        @ClassRule
+        @JvmField
+        val rxSchedulerRule = RxSchedulerRule()
     }
 }
